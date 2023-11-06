ab7a8a9cebed524efa89195f5b64871b0ab18907
Tim Diliberto <timothydiliberto19@gmail.com>
2023-11-05 21:05:10 -0800

    Started drawing background of window; removed extra variables

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -9,22 +9,15 @@ int main()
     int width = 800;
     int height = 450;
 
-    
-    // Additional data types, for reference
-    float root_beer = 1.99; // holds 6-7 characters
-    double cheese_burger{5.99}; // holds 15-16 characters
-    bool shouldHaveLunch{}; // initialized to false
-    // operator quiz
-    bool equal{4 == 9}; // false
-    bool notEqual{4 != 9}; // true
-    bool less{4 < 9}; // true
-    bool greater{4 > 9}; // false
-    bool lessEqual{4 <= 9}; // true
-    bool greaterEqual{4 >= 9}; // false
-    
-    
     // Initialize Popup window with specified dimensions and title
     InitWindow(width, height, "Axe Game");
+
+    while (true)
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
 
     return 0;
 }
