f81183d2c36ba6dfc7bea0c7779002c817dc0113
Tim Diliberto <timothydiliberto19@gmail.com>
2023-11-05 20:52:37 -0800

    Added return 0; took an operator quiz

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -14,9 +14,17 @@ int main()
     float root_beer = 1.99; // holds 6-7 characters
     double cheese_burger{5.99}; // holds 15-16 characters
     bool shouldHaveLunch{}; // initialized to false
+    // operator quiz
+    bool equal{4 == 9}; // false
+    bool notEqual{4 != 9}; // true
+    bool less{4 < 9}; // true
+    bool greater{4 > 9}; // false
+    bool lessEqual{4 <= 9}; // true
+    bool greaterEqual{4 >= 9}; // false
     
     
-    // Initialize Popup window with specified dimensions and
-    // title
+    // Initialize Popup window with specified dimensions and title
     InitWindow(width, height, "Axe Game");
+
+    return 0;
 }
