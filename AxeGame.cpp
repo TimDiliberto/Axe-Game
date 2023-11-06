55f745b0202d10fb3bfd5176e09d9cf6775fd5da
Tim Diliberto <timothydiliberto19@gmail.com>
2023-11-05 20:37:01 -0800

    Just some type examples

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -8,6 +8,13 @@ int main()
     // Initialize WIDTH and HEIGHT variables
     int width = 800;
     int height = 450;
+
+    
+    // Additional data types, for reference
+    float root_beer = 1.99; // holds 6-7 characters
+    double cheese_burger{5.99}; // holds 15-16 characters
+    bool shouldHaveLunch{}; // initialized to false
+    
     
     // Initialize Popup window with specified dimensions and
     // title
