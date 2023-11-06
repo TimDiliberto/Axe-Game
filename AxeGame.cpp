822196862aec897e50161f1dbaf7d204ce706c00
Tim Diliberto <timothydiliberto19@gmail.com>
2023-11-05 22:10:07 -0800

    Added some baby physics for the axe, but alas no immersive bounce

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -10,6 +10,9 @@ int main()
     int winHeight{900};
     InitWindow(winWidth, winHeight, "Axe Game");
 
+    // Initialize physics variables
+    float gravity = 9.81;
+
     // Initialize circle data
     int circleX = 400;
     int circleY = 400;
@@ -21,6 +24,7 @@ int main()
     int axeY = 0;
     int axeWidth = 50;
     int axeHeight = 50;
+    float axeMass = 1.0;
 
     // Ideal frames per second
     SetTargetFPS(60);
@@ -38,7 +42,8 @@ int main()
         //DrawRectangle(axeX+200, axeY, axeWidth, axeHeight, BROWN);
 
         // Move the axe
-        axeY += speed;
+        axeY += axeMass * gravity;
+        if (axeY >= (900-axeHeight) || axeY <= 0) { axeMass = -axeMass; }
 
         // Move circle smoothly with WASD and add barrier
         if (IsKeyDown(KEY_W) && circleY > 0 + radius)
